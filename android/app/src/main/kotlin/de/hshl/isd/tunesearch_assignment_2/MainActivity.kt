004465e15ab9e13a4bbf7ddf25c8bc03096216e7
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-12-17 17:19:04 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/de/hshl/isd/tunesearch_assignment_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package de.hshl.isd.tunesearch_assignment_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:tunesearch_assignment_2/my_app.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
